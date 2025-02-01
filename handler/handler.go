8c2d53d5a1f4f0259288dbab681b75d2832e1d96
ss-loves-AC <ssompu88@gmail.com>
2025-02-01 19:49:22 +0530

    implemented Get and Search Handler

--- handler/handler.go
+++ handler/handler.go
@@ -12,9 +12,26 @@ type Handler struct {
 }
 
 func (h *Handler) GetVideos(c *gin.Context) {
+	videos, err := h.DB.GetVideos(c)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, videos)
 }
 
 func (h *Handler) SearchVideos(c *gin.Context) {
+	query := c.Query("query")
+	if query == "" {
+		c.JSON(400, gin.H{"error": "query parameter is required"})
+		return
+	}
+	videos, err := h.DB.SearchVideos(c, query)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, videos)
 }
 
 func NewHandler(db storage.DB , cache storage.Cache) *Handler {
