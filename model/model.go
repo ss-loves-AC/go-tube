37f12ffec7035088e7f30d9d556fb3ff0746884e
ss-loves-AC <ssompu88@gmail.com>
2025-02-02 01:52:31 +0530

    simplify video model

--- model/model.go
+++ model/model.go
@@ -7,7 +7,6 @@ type Video struct {
 	VideoID     string    `gorm:"type:VARCHAR(255);uniqueIndex;not null" json:"video_id"` 
 	Title       string    `gorm:"not null" json:"title"`
 	Description string    `gorm:"type:text" json:"description"`
-	URL         string    `json:"url"`
 	Thumbnail   string    `json:"thumbnail"`
 	PublishedAt time.Time `gorm:"not null;index" json:"published_at"`
 }
