ffa5f905488efa67b14be4ea0f92434587fb9c20
ss-loves-AC <ssompu88@gmail.com>
2025-02-01 16:45:57 +0530

    Implement searchAndStoreVideos method to call YouTube API and insert videos into DB

--- go.mod
+++ go.mod
@@ -4,22 +4,33 @@ go 1.23.2
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	google.golang.org/api v0.219.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
 )
 
 require (
+	cloud.google.com/go/auth v0.14.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.7 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -31,11 +42,18 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
+	go.opentelemetry.io/otel v1.32.0 // indirect
+	go.opentelemetry.io/otel/metric v1.32.0 // indirect
+	go.opentelemetry.io/otel/trace v1.32.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/oauth2 v0.25.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250124145028-65684f501c47 // indirect
+	google.golang.org/grpc v1.70.0 // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- service/youtube.go
+++ service/youtube.go
@@ -2,26 +2,35 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"go-tube/model"
 	"go-tube/storage"
 	"log"
 	"time"
+
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 )
 
 type YoutubeService struct {
-	DB      storage.DB
-	Cache   storage.Cache
-	Query   string
-	APIKeys []string
-	round   int
+	DB            storage.DB
+	Cache         storage.Cache
+	Query         string
+	APIKeys       []string 
+	round         int // lets keep track of the current API key index and use round robin technique to switch between keys
+	errorCount    int 
+	maxErrorCount int
 }
 
-func NewYoutubeService(db storage.DB, cache storage.Cache, query string , apiKeys []string) *YoutubeService {
+func NewYoutubeService(db storage.DB, cache storage.Cache, query string, apiKeys []string) *YoutubeService {
 	return &YoutubeService{
-		DB:    db,
-		Cache: cache,
-		Query: query,
-		APIKeys: apiKeys,
-		round: 0,
+		DB:            db,
+		Cache:         cache,
+		Query:         query,
+		APIKeys:       apiKeys,
+		round:         0,
+		errorCount:    0,
+		maxErrorCount: 20,
 	}
 }
 
@@ -36,11 +45,74 @@ func (y *YoutubeService) Start(ctx context.Context, interval time.Duration) {
 			log.Println("Stopping youtube service")
 			return
 		case <-ticker.C:
-			log.Println("Searching videos for query", y.Query)
-			y.searchAndStoreVideos()
+			if err := y.searchAndStoreVideos(ctx); err != nil {
+				log.Printf("Error during video search and store: %v\n", err)
+				y.errorCount++
+				if y.errorCount >= y.maxErrorCount {
+					log.Println("Max error count reached. Stopping youtube service")
+					_, cancel := context.WithCancel(ctx)
+					cancel()
+					return
+				}
+			} else {
+				y.errorCount = 0
+			}
 		}
 	}
 }
 
-func (y *YoutubeService) searchAndStoreVideos() {
+func (y *YoutubeService) searchAndStoreVideos(ctx context.Context) error {
+	apiKey := y.getNextAPIKey()
+	if apiKey == "" {
+		return fmt.Errorf("no API keys available")
+	}
+
+	service, err := youtube.NewService(ctx, option.WithAPIKey(apiKey))
+	if err != nil {
+		log.Println("Failed to create YouTube service:", err)
+		return fmt.Errorf("failed to create YouTube service: %v", err)
+	}
+
+	call := service.Search.List([]string{"id", "snippet"}).
+		Q(y.Query).
+		MaxResults(10)
+	response, err := call.Do()
+	if err != nil {
+		log.Printf("Failed to search videos: %v", err)
+		return fmt.Errorf("failed to search videos: %v", err)
+	}
+
+	for _, item := range response.Items {
+		publishedAt, err := time.Parse(time.RFC3339, item.Snippet.PublishedAt)
+		if err != nil {
+			log.Printf("Failed to parse PublishedAt for video %s: %v\n", item.Id.VideoId, err)
+			continue
+		}
+
+		video := model.Video{
+			VideoID:     item.Id.VideoId,
+			Title:       item.Snippet.Title,
+			Description: item.Snippet.Description,
+			Thumbnail:   item.Snippet.Thumbnails.Default.Url,
+			PublishedAt: publishedAt,
+		}
+
+		if err := y.DB.InsertVideo(ctx, video); err != nil {
+			log.Printf("Failed to insert video %s: %v\n", video.VideoID, err)
+			continue
+		}
+	}
+
+	log.Printf("Successfully fetched and stored %d videos\n", len(response.Items))
+	return nil
+}
+
+func (y *YoutubeService) getNextAPIKey() string {
+	if len(y.APIKeys) == 0 {
+		return ""
+	}
+
+	apiKey := y.APIKeys[y.round]
+	y.round = (y.round + 1) % len(y.APIKeys) 
+	return apiKey
 }
